d18acab804813c7e9b8d7ebfaf3e114930cf075b
p20102 <yanicksilvan.romere@css.ch>
2024-08-29 21:37:30 +0200

    + fix storage

--- modules/StorageHandler.py
+++ modules/StorageHandler.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import requests
 
 from models.Environment import BASE_URL_STORAGE
@@ -10,11 +12,8 @@ def find_lowest_item():
         data = response.json().get("hold")[y]
         for x in reversed(range(12)):
             if data[x] is not None:
-                print(used_positions)
                 if Vector2(x, y) in used_positions:
-
                     continue
-                print(Vector2(x, y))
                 return Vector2(x, y)
 
 def find_lowest_position():
@@ -25,28 +24,32 @@ def find_lowest_position():
             if data[j] is None:
                 return Vector2(j, n)
             else:
-                used_positions.append(Vector2(j, n))
+                if Vector2(j, n) not in used_positions:
+                    used_positions.append(Vector2(j, n))
+
 
 def move_lowest_item_to_lowest_position():
     position = find_lowest_position()
     lowest_item = find_lowest_item()
-    print(lowest_item)
 
-    while lowest_item.x > position.x:
-        out_data = {
-            "a": {"x": lowest_item.x, "y": lowest_item.y},
-            "b": {"x": lowest_item.x - 1, "y": lowest_item.y}
-        }
-        lowest_item.x = lowest_item.x - 1
-        requests.post(f"{BASE_URL_STORAGE}swap_adjacent", json=out_data).json()
-
-    while lowest_item.x < position.x:
-        out_data = {
-            "a": {"x": lowest_item.x, "y": lowest_item.y},
-            "b": {"x": lowest_item.x + 1, "y": lowest_item.y}
-        }
-        lowest_item.x = lowest_item.x + 1
-        requests.post(f"{BASE_URL_STORAGE}swap_adjacent", json=out_data).json()
+    if lowest_item.x > position.x:
+        while lowest_item.x > position.x:
+            out_data = {
+                "a": {"x": lowest_item.x, "y": lowest_item.y},
+                "b": {"x": lowest_item.x - 1, "y": lowest_item.y}
+            }
+            lowest_item.x = lowest_item.x - 1
+            print(requests.post(f"{BASE_URL_STORAGE}swap_adjacent", json=out_data).json())
+            sleep(1)
+    elif lowest_item.x < position.x:
+        while lowest_item.x < position.x:
+            out_data = {
+                "a": {"x": lowest_item.x, "y": lowest_item.y},
+                "b": {"x": lowest_item.x + 1, "y": lowest_item.y}
+            }
+            lowest_item.x = lowest_item.x + 1
+            print(requests.post(f"{BASE_URL_STORAGE}swap_adjacent", json=out_data).json())
+            sleep(1)
 
     while lowest_item.y < position.y:
         out_data = {
@@ -54,7 +57,8 @@ def move_lowest_item_to_lowest_position():
             "b": {"x": lowest_item.x, "y": lowest_item.y + 1}
         }
         lowest_item.y = lowest_item.y + 1
-        requests.post(f"{BASE_URL_STORAGE}swap_adjacent", json=out_data).json()
+        print(requests.post(f"{BASE_URL_STORAGE}swap_adjacent", json=out_data).json())
+        sleep(1)
 
 for j in range(100):
     move_lowest_item_to_lowest_position()
