9ab9e9447214fe3a3b0a16f4ec2020970ed06845
prouge <justcrout@gmail.com>
2024-08-31 23:40:19 +0200

    refactoring

--- modules/ShopHandler.py
+++ modules/ShopHandler.py
@@ -5,21 +5,22 @@ from models.Item import ItemContainer
 from models.Station import Station
 
 
-def buy_item(station: Station, item_container: ItemContainer):
-    data = {"station": station.name, "what": item_container.item.name, "amount": item_container.amount}
+def __send_request(action: str, station: Station, item_container: ItemContainer) -> tuple:
+    data = {
+        "station": station.name,
+        "what": item_container.item.name,
+        "amount": item_container.amount
+    }
     try:
-        response = requests.post(f"{BASE_URL_STORE}buy", json=data)
-        print('LOGGER: ' + response.text)
+        response = requests.post(f"{BASE_URL_STORE}{action}", json=data)
         return response.status_code, response.text
     except requests.exceptions.RequestException as e:
-        return None, str(e)
+        raise e
+
+
+def buy_item(station: Station, item_container: ItemContainer):
+    return __send_request("buy", station, item_container)
 
 
 def sell_item(station: Station, item_container: ItemContainer):
-    data = {"station": station.name, "what": item_container.item.name, "amount": item_container.amount}
-    print(data)
-    try:
-        response = requests.post(f"{BASE_URL_STORE}sell", json=data)
-        return response.status_code, response.text
-    except requests.exceptions.RequestException as e:
-        return None, str(e)
+    return __send_request("sell", station, item_container)
