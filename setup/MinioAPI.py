0da1e97d0d151d1c81624934e666373befbd4542
prouge <justcrout@gmail.com>
2024-09-07 19:39:23 +0200

    improvement

--- setup/MinioAPI.py
+++ setup/MinioAPI.py
@@ -40,7 +40,7 @@ s3_client = boto3.client(
 
 @app.route('/<station>/receive', methods=['POST'])
 def receive(station):
-    return jsonify({"kind": "success", "messages": "result"}), 200
+    return jsonify({"kind": "success", "messages": f"Received station: {station}"}), 200
 
 def zurro_rest():
     try:
