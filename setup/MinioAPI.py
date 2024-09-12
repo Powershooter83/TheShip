953c6cacfd987d4ea4b8283f71fb2e51300a2a0e
prouge <justcrout@gmail.com>
2024-09-12 15:04:08 +0200

    improvement minio

--- setup/MinioAPI.py
+++ setup/MinioAPI.py
@@ -4,60 +4,37 @@ import json
 import uuid
 
 import requests
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import boto3
 
 from models.Station import StationEnum
 
 app = Flask(__name__)
 
-@app.route('/<station>/receive', methods=['POST'])
-def receive(station):
-    data = zurro_rest()
-    print(transform_messages(json.loads(data.text).get('received_messages')))
-
-    return jsonify({"kind": "success", "messages": f"Received station: {station}"})
+def __find_station_by_name(station_name):
+    for station in StationEnum:
+        if station.value.name == station_name:
+            return station
 
 
-@app.route('/<station>/send', methods=['POST'])
-def send(station):
-
-   if station == 'Zurro Station':
-       print('ZURRO STATION')
-   print(station)
-
-   return None
-
-def zurro_rest():
+def __zurro_interface_send(station: StationEnum, msg):
     try:
-        return requests.post(f"{StationEnum.ZURRO.value.get_url()}receive")
-    except requests.exceptions.RequestException as e:
-        raise e
-
-data = zurro_rest()
-
-
-
-
-
-
-
-def transform_messages(received_messages):
-    transformed_messages = []
-
-    for item in received_messages:
-        dest = item["dest"]
-        msg = item["msg"]
-
         decoded_bytes = base64.b64decode(msg)
         decoded_str = decoded_bytes.decode('utf-8')
 
-        transformed_messages.append({
-            "destination": dest,
-            "data": decoded_str
-        })
+        data = {"src": station.value.name, "msg": decoded_str}
+        return requests.post(f"{StationEnum.ZURRO.value.get_url()}send", json=data)
+    except requests.exceptions.RequestException as e:
+        raise e
 
-    return transformed_messages
+@app.route('/<station>/send', methods=['POST'])
+def send(station_name):
+    station = __find_station_by_name(station_name)
+    data = request.json
+    source_station = __find_station_by_name(data['source'])
+    match station:
+        case StationEnum.ZURRO:
+            return __zurro_interface_send(source_station, data['data'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2023, debug=True)
