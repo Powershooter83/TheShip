896de7802a19f512439600718542105934345e89
prouge <justcrout@gmail.com>
2024-09-12 21:54:39 +0200

    add station

--- setup/MinioAPI.py
+++ setup/MinioAPI.py
@@ -51,7 +51,9 @@ def __artemis_interface_receive(destination_station: Station):
             msg = json.loads(data.decode('utf-8')).get('message')
             print(msg, file=sys.stdout)
             print(json.loads(msg).get('data'), file=sys.stdout)
-            # decoded_bytes = base64.b64decode(msg)
+            decoded_bytes = base64.b64decode(json.loads(msg).get('data'))
+            print(decoded_bytes, file=sys.stdout)
+            print(list(decoded_bytes), file=sys.stdout)
 #            messages.append({"destination": "Azura Station", "data": list(decoded_bytes)})
     return {"kind": "success", "messages": messages}
 
