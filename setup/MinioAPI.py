cd6a198803107cb664c44ca21fafac2c6f2ad69c
prouge <justcrout@gmail.com>
2024-09-13 10:37:05 +0200

    add station

--- setup/MinioAPI.py
+++ setup/MinioAPI.py
@@ -34,20 +34,17 @@ def __core_interface_send(station: Station, msg):
     requests.post(f"{StationEnum.CORE.value.get_url()}send", json=data)
 
 def __azura_interface_receive(destination_station: Station):
-    data = requests.get(f"{StationEnum.AZURA.value.get_url()}messages_for_other_stations")
-    print(data, file=sys.stdout)
-    print(data.text, file=sys.stdout)
-    # received_messages = json.loads(requests.post(f"{StationEnum.AZURA.value.get_url()}messages_for_other_stations").text).get(
-    #     "received_messages")
-    # messages = []
-    # for message in received_messages:
-    #     dest = message.get("dest")
-    #
-    #     if dest == destination_station.name:
-    #         msg = message.get("base64data")
-    #         decoded_bytes = base64.b64decode(msg)
-    #         messages.append({"destination": destination_station.name, "data": list(decoded_bytes)})
-    return {"kind": "success", "messages": []}
+    received_messages = json.loads(requests.get(f"{StationEnum.AZURA.value.get_url()}messages_for_other_stations").text).get(
+        "received_messages")
+    messages = []
+    for message in received_messages:
+        dest = message.get("dest")
+
+        if dest == destination_station.name:
+            msg = message.get("base64data")
+            decoded_bytes = base64.b64decode(msg)
+            messages.append({"destination": destination_station.name, "data": list(decoded_bytes)})
+    return {"kind": "success", "messages": messages}
 
 
 def __zurro_interface_receive(destination_station: Station):
