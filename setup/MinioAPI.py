4f96f7a2ba9bc0208148ab46bbf333d2c1fa8104
prouge <justcrout@gmail.com>
2024-09-13 11:37:48 +0200

    add station

--- setup/MinioAPI.py
+++ setup/MinioAPI.py
@@ -91,8 +91,8 @@ def __aurora_interface_receive(destination_station: Station):
             decoded_data =response_msg.decode('utf-8')
             decoded_data= decoded_data.strip()
             print(decoded_data, file=sys.stdout)
-            decoded_bytes = base64.b64decode(json.loads(decoded_data))
-            messages.append({"destination": destination_station.name, "data": list(decoded_bytes)})
+            # decoded_bytes = base64.b64decode(json.loads(decoded_data))
+            # messages.append({"destination": destination_station.name, "data": list(decoded_bytes)})
     return {"kind": "success", "messages": messages}
 
 def __core_interface_receive(destination_station: Station):
