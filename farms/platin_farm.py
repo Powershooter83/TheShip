068eb09912fba0bf0e22afe24cb7379c4cab5eff
prouge <justcrout@gmail.com>
2024-09-13 08:54:39 +0200

    add station

--- farms/platin_farm.py
+++ farms/platin_farm.py
@@ -1,7 +1,9 @@
 from models.Item import Item
-from models.Station import StationEnum, ResourceEnum
+from models.Station import StationEnum, ResourceEnum, Station
+from models.Vector2 import Vector2
 from modules.FarmManager import FarmManager
 
 
-platin_farm = FarmManager(StationEnum.CORE.value, ResourceEnum.PLATIN.value, Item.PLATIN, True)
+platin_farm = FarmManager(StationEnum.CORE.value,
+                        Station("Platin Mountain", Vector2(50000, 77457), None), Item.PLATIN, True)
 platin_farm.start()
