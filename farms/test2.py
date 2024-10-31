46bc598f0548dda154b221dac3e1c92953c6f54e
JanisM05 <116655274+JanisM05@users.noreply.github.com>
2024-10-31 15:19:54 +0100

    add test2

---
+++ farms/test2.py
@@ -0,0 +1,16 @@
+import requests from flask
+import Flask
+
+app = (Flask(__name__)
+
+@app.route(rule="/", methods=["GET"]))
+def get_data():
+    try:
+        response = requests.get("http://192.168.100.21:2038/data")
+        json = response.json()
+        return {"data": json.get("result")}
+    except Exception as e:
+        return {"error": str(e)}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=2101)
